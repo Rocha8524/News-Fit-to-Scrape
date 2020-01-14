3b91c41496dd14092c46809f992e64539aebc5fb
Alex Rocha <rocha8524@gmail.com>
2020-01-14 18:03:58 -0500

    Fixing application

--- models/article.js
+++ models/article.js
@@ -6,12 +6,10 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   headline: {
     type: String,
     unique: true
   },
-  // `link` is required and of type String
   summary: String,
   storyUrl: String,
   imgUrl: String,
@@ -20,7 +18,7 @@ var ArticleSchema = new Schema({
     type: Boolean,
     default: false
   },
-  // `note` is an object that stores a Note id
+
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
   note: {
--- server.js
+++ server.js
@@ -1,62 +1,59 @@
-//dependencies
-const express = require('express'),
-      mongoose = require('mongoose'),
-      exphbs = require('express-handlebars'),
-      bodyParser = require('body-parser'),
-      logger = require('morgan'),
-      path = require('path'),
-      favicon = require('serve-favicon');
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
 
-//initializing the app
-const app = express();
-
-//setting up the database
-const config = require('./config/database');
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
-mongoose
-  .connect(config.database, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then( result => {
-    console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
-  })
-  .catch(err => console.log('There was an error with your connection:', err));
 
-//setting up favicon middleware
-app.use(favicon(path.join(__dirname, 'public', 'assets/img/favicon.ico')));
+var PORT = process.env.PORT || 8080;
 
-//setting up Morgan middleware
-app.use(logger('dev'));
+// Initialize Express
+var app = express();
 
-//setting up body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//setting up handlebars middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// Serve static content
+app.use(express.static("public"));
 
-//setting up the static directory
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/articles',express.static(path.join(__dirname, 'public')));
-app.use('/notes',express.static(path.join(__dirname, 'public')));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//setting up routes
-const index = require('./routes/index'),
-      articles = require('./routes/articles'),
-      notes = require('./routes/notes'),
-      scrape = require('./routes/scrape');
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper_controller.js");
 
-app.use('/', index);
-app.use('/articles', articles);
-app.use('/notes', notes);
-app.use('/scrape', scrape);
+app.use("/", routes);
 
-//starting server
-const PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function (error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function () {
+  console.log("Mongoose connection successful.");
+});
+
+// Listen on port 8080
 app.listen(PORT, function () {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log("App running on PORT " + PORT);
 });
